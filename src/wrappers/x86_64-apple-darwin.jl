997770990cacaa400a1736de1037fab9412c2c7a
jlbuild <juliabuildbot@gmail.com>
2023-05-27 22:34:41 +0000

    HiGHS_jll build 1.5.3+0

--- src/wrappers/x86_64-apple-darwin.jl
+++ src/wrappers/x86_64-apple-darwin.jl
@@ -9,7 +9,7 @@ function __init__()
     JLLWrappers.@generate_init_header(CompilerSupportLibraries_jll)
     JLLWrappers.@init_library_product(
         libhighs,
-        "lib/libhighs.1.5.1.dylib",
+        "lib/libhighs.1.5.3.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
