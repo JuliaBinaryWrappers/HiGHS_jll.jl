19c3e1a258328f73bc453bb11e0760055ec3c858
jlbuild <juliabuildbot@gmail.com>
2025-10-26 22:30:11 +0000

    HiGHS_jll build 1.12.0+0

--- src/wrappers/aarch64-linux-gnu-cxx11.jl
+++ src/wrappers/aarch64-linux-gnu-cxx11.jl
@@ -3,11 +3,13 @@ export highs, libhighs
 
 using CompilerSupportLibraries_jll
 using Zlib_jll
+using METIS_jll
+using OpenBLAS32_jll
 JLLWrappers.@generate_wrapper_header("HiGHS")
 JLLWrappers.@declare_library_product(libhighs, "libhighs.so.1")
 JLLWrappers.@declare_executable_product(highs)
 function __init__()
-    JLLWrappers.@generate_init_header(CompilerSupportLibraries_jll, Zlib_jll)
+    JLLWrappers.@generate_init_header(CompilerSupportLibraries_jll, Zlib_jll, METIS_jll, OpenBLAS32_jll)
     JLLWrappers.@init_library_product(
         libhighs,
         "lib/libhighs.so",
