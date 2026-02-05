70068c4e1c08f3151c47b3b28391b2ee03afcc42
jlbuild <juliabuildbot@gmail.com>
2026-02-05 02:44:27 +0000

    HiGHS_jll build 1.13.0+0

--- src/HiGHS_jll.jl
+++ src/HiGHS_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("HiGHS")
-JLLWrappers.@generate_main_file("HiGHS", UUID("8fd58aa0-07eb-5a78-9b36-339c94fd15ea"))
+JLLWrappers.@generate_main_file("HiGHS", Base.UUID("8fd58aa0-07eb-5a78-9b36-339c94fd15ea"))
 end  # module HiGHS_jll
--- src/wrappers/x86_64-apple-darwin.jl
+++ src/wrappers/x86_64-apple-darwin.jl
@@ -3,16 +3,15 @@ export highs, libhighs
 
 using CompilerSupportLibraries_jll
 using Zlib_jll
-using METIS_jll
-using OpenBLAS32_jll
+using libblastrampoline_jll
 JLLWrappers.@generate_wrapper_header("HiGHS")
 JLLWrappers.@declare_library_product(libhighs, "@rpath/libhighs.1.dylib")
 JLLWrappers.@declare_executable_product(highs)
 function __init__()
-    JLLWrappers.@generate_init_header(CompilerSupportLibraries_jll, Zlib_jll, METIS_jll, OpenBLAS32_jll)
+    JLLWrappers.@generate_init_header(CompilerSupportLibraries_jll, Zlib_jll, libblastrampoline_jll)
     JLLWrappers.@init_library_product(
         libhighs,
-        "lib/libhighs.1.12.dylib",
+        "lib/libhighs.1.13.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
