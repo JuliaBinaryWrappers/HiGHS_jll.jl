8e2e4096392b5a197d26537b8617ebbc38a3cc37
jlbuild <juliabuildbot@gmail.com>
2026-04-06 22:12:16 +0000

    HiGHS_jll build 1.14.0+0

--- src/wrappers/aarch64-apple-darwin.jl
+++ src/wrappers/aarch64-apple-darwin.jl
@@ -11,7 +11,7 @@ function __init__()
     JLLWrappers.@generate_init_header(CompilerSupportLibraries_jll, Zlib_jll, libblastrampoline_jll)
     JLLWrappers.@init_library_product(
         libhighs,
-        "lib/libhighs.1.13.dylib",
+        "lib/libhighs.1.14.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
